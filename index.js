96a3f437eff4995e1b644aa4b61ee7896e7be7d8
Sakshyam Gurau <86282314+Sakshyam059@users.noreply.github.com>
2022-08-31 22:30:28 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -34,3 +34,28 @@ document.addEventListener('mouseup', function(e){
 
     }
 });
+
+
+
+function login(){
+    var pw = document.getElementById("password").value;  
+  //check empty password field  
+  if(pw == "") {  
+     document.getElementById("notify").innerHTML = "Fill the password please!";  
+     return false;  
+  }  
+   
+ //minimum password length validation  
+  if(pw.length < 8) {  
+     document.getElementById("notify").innerHTML = "Password length must be atleast 8 characters";  
+     return false;  
+  }  
+  
+//maximum length of password validation  
+  if(pw.length > 15) {  
+     document.getElementById("notify").innerHTML = "Password length must not exceed 15 characters";  
+     return false;  
+  } else {  
+     alert("Password is correct");  
+  } 
+}
